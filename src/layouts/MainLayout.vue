8945a8877aa36796069b2cc61e6f6d54bf03cddd
Jesse Sopel <jesse.sopel@gmail.com>
2024-06-20 22:57:26 -0400

    feat: Add main layout and extremely basic header

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Link } from "@inertiajs/vue3"
+import Toolbar from "primevue/toolbar";
+import Avatar from "primevue/avatar";
+
+import { User } from "../types/users.ts";
+
+defineProps<{ user?: User }>()
+</script>
+
+<template>
+  <main>
+    <header>
+      <Toolbar>
+        <template #start>
+          <div class="flex items-center gap-2">
+            <Link href="/">
+              <Image src="/static/images/recipe-logo.svg" alt="" :width="50" />
+              <span class="flex">Recipes</span>
+            </Link>
+          </div>
+        </template>
+        <template #end>
+          <div class="flex items-center gap-2">
+            <Avatar v-if="user" :image="user.profile_image_url" size="large" shape="circle" />
+            <Link v-else href="/admin/login/">Login</Link>
+          </div>
+        </template>
+      </Toolbar>
+    </header>
+    <article>
+      <slot />
+    </article>
+  </main>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,13 @@
+<script setup lang="ts">
+import { User } from "../types/users.ts";
+
+defineProps<{ user?: User }>();
+</script>
+
 <template>
   <div class="text-center h-screen flex justify-center flex-col items-center p-4">
-    <h1 class="text-3xl font-bold">Welcome to the Recipe Repo</h1>
+    <h1 class="text-3xl font-bold" v-if="user">Welcome {{ user.first_name }}</h1>
+    <h1 v-else>Welcome. Please Login!</h1>
     <h2 class="text-green-600 mb-5">This is still an empty project</h2>
   </div>
 </template>
