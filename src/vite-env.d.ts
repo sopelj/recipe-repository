ba6abe8e74b0497294f8b1dc5ab21d4eb4b627f9
Jesse Sopel <jesse.sopel@gmail.com>
2024-08-11 13:04:11 -0400

    feat: Add vite PWA to try and better handle PWA mode

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_APP_TITLE_SHORT: string;
+  readonly VITE_APP_THEME_COLOUR: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,73 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 import Components from "unplugin-vue-components/vite";
 import { PrimeVueResolver } from "@primevue/auto-import-resolver";
 import { fileURLToPath } from "node:url";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), Components({ resolvers: [PrimeVueResolver()] })],
-  root: resolve("./src"),
-  base: "/static/",
-  server: {
-    host: "localhost",
-    port: 5173,
-    open: false,
-    watch: {
-      usePolling: true,
-      disableGlobbing: false,
-    },
-  },
-  resolve: {
-    extensions: [".vue", ".ts", ".js", ".json"],
-    alias: [{ find: "@", replacement: fileURLToPath(new URL("./src", import.meta.url)) }],
-  },
-  build: {
-    outDir: resolve("./static/dist"),
-    assetsDir: "",
-    manifest: "manifest.json",
-    emptyOutDir: true,
-    target: "es2022",
-    rollupOptions: {
-      input: {
-        main: resolve("./src/main.ts"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      vue(),
+      Components({ resolvers: [PrimeVueResolver()] }),
+      VitePWA({
+        registerType: "autoUpdate",
+        injectRegister: "auto",
+        includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+        manifest: {
+          name: env.VITE_APP_TITLE || "Recipe Repository",
+          short_name: env.VITE_APP_TITLE_SHORT || "Recipes",
+          description: env.VITE_APP_DESCRIPTION || "A repository for your favourite recipes.",
+          theme_color: env.VITE_APP_THEME_COLOUR,
+          display: "standalone",
+          start_url: "/",
+          icons: [
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+      }),
+    ],
+    root: resolve("./src"),
+    base: "/static/",
+    server: {
+      host: "localhost",
+      port: 5173,
+      open: false,
+      watch: {
+        usePolling: true,
+        disableGlobbing: false,
       },
-      output: {
-        chunkFileNames: undefined,
+    },
+    resolve: {
+      extensions: [".vue", ".ts", ".js", ".json"],
+      alias: [{ find: "@", replacement: fileURLToPath(new URL("./src", import.meta.url)) }],
+    },
+    build: {
+      outDir: resolve("./static/dist"),
+      assetsDir: "",
+      manifest: "manifest.json",
+      emptyOutDir: true,
+      target: "es2022",
+      rollupOptions: {
+        input: {
+          main: resolve("./src/main.ts"),
+        },
+        output: {
+          chunkFileNames: undefined,
+        },
       },
     },
-  },
+  };
 });
