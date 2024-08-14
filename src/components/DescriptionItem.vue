6f3cac719bf75319ece9201ebfb28bdb5d5429ae
Jesse Sopel <jesse.sopel@gmail.com>
2024-08-13 21:16:19 -0400

    fix: Align name properly

--- src/components/DescriptionItem.vue
+++ src/components/DescriptionItem.vue
@@ -5,7 +5,7 @@ defineProps<{ label: string }>();
 <template>
   <div class="flex align-items-center py-3">
     <div class="w-1/4 text-500 font-medium">{{ label }}</div>
-    <div class="w-3/4">
+    <div class="w-3/4 flex items-center">
       <slot />
     </div>
   </div>
