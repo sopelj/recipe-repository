60a10cb2e9ad19ddaf6e3dd086ccabfb93dd3460
Jesse Sopel <jesse.sopel@gmail.com>
2024-08-18 13:33:03 -0400

    feat: Run ESLint via yarn instead of with mirror

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig, loadEnv } from "vite";
-import { resolve } from "path";
-import { PrimeVueResolver } from "@primevue/auto-import-resolver";
-import { fileURLToPath } from "node:url";
-import { VitePWA } from "vite-plugin-pwa";
-import VuePlugin from "@vitejs/plugin-vue";
-import Components from "unplugin-vue-components/vite";
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
+import { PrimeVueResolver } from "@primevue/auto-import-resolver";
+import VuePlugin from "@vitejs/plugin-vue";
+import { fileURLToPath } from "node:url";
+import { resolve } from "path";
+import Components from "unplugin-vue-components/vite";
+import { defineConfig, loadEnv } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
